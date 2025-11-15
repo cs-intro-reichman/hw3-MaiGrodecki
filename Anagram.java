a81ed0b54f68642605dbea8e6e47405d44dbdb49
Mai Grodecki <mai.grodecki@post.runi.ac.il>
2025-11-16 01:32:22 +0200

    task 3 1-3 v5

--- Anagram.java
+++ Anagram.java
@@ -11,7 +11,7 @@ public class Anagram {
 		System.out.println(preProcess("What? No way!!!"));
 		
 		// Tests the randomAnagram function.
-		System.out.println("silent and " + randomAnagram("silent") + " are anagrams.");
+		//System.out.println("silent and " + randomAnagram("silent") + " are anagrams.");
 		
 		// Performs a stress test of randomAnagram 
 		String str = "1234567";
@@ -28,22 +28,48 @@ public class Anagram {
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
-		// Replace the following statement with your code
-		return false;
+		str1 = preProcess(str1);
+		str2 = preProcess(str2);
+		if(str1.length() != str2.length()){
+			return false;
+		}
+		String temp = str2;
+		for (int i = 0; i < str1.length(); i++){
+				 int index = temp.indexOf(str1.charAt(i));
+				 if ( index == -1){
+					return false;
+				 }
+				 temp = temp.substring(0, index) + temp.substring(index + 1);
+			}
+		return true;
 	}
 	   
 	// Returns a preprocessed version of the given string: all the letter characters are converted
 	// to lower-case, and all the other characters are deleted, except for spaces, which are left
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
-		// Replace the following statement with your code
-		return "";
+		str = str.toLowerCase();
+		for (int i = 0; i < str.length(); i++){
+			if (Character.isLetter(str.charAt(i)) == false){
+				str = str.substring(0, i) + str.substring(i + 1);
+				i--;
+			}
+		}
+		return str;
 	} 
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
 	public static String randomAnagram(String str) {
-		// Replace the following statement with your code
-		return "";
+		String newWord = "";
+		char selected;
+		for(int i = 0; i < str.length(); i++){
+			selected = str.charAt((int)(Math.random() * str.length() - 1));
+			str = str.substring(0, i) + str.substring(i + 1);
+			newWord = newWord + selected;
+		}
+		return newWord;
+
 	}
 }
+
